fb1923c029e604272ceb7fc32b845be637261b13
Brandon Chung <brandon.k.chung@gmail.com>
2022-05-30 23:27:47 -0500

    change up homepage text

--- src/views/Home.tsx
+++ src/views/Home.tsx
@@ -3,8 +3,11 @@ import { Text } from 'components';
 export const Home = () => (
   <>
     <Text variant="h1" className="prose prose-slate dark:prose-invert ">
-      Positive affirmations are useless without objective truth.{' '}
-      <em>Here's what God says about you.</em>
+      Positive affirmations are more effective with objective truth.{' '}
+      <em>
+        Here's what God says about{' '}
+        <span className="text-purple-500 dark:text-purple-600">you</span>.
+      </em>
     </Text>
   </>
 );
